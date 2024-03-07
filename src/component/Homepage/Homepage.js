4e95641472a6612ca444b6884ebeb6fdbec5c187
Apiwit22 <apiwit.n@ku.th>
2024-03-07 19:06:43 +0700

    shirtstore

---
+++ src/component/Homepage/Homepage.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var loginButton = document.querySelector(".btnLogin-popup");
+    loginButton.addEventListener("click", function() {
+        window.location.href = "http://127.0.0.1:5500/src/component/Login/index.html"; // เปลี่ยนไปยังหน้า loginpage.html
+    });
+
+    // เพิ่มโค้ด JavaScript อื่น ๆ ตามที่ต้องการ
+});
+
