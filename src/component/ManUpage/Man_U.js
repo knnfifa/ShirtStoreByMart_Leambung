de18d7ceb2609c427fca7b93c49251c2716650ec
Thitipat Charonekitpasuk <owen2546@gmail.com>
2024-03-10 20:23:06 +0700

    white text

--- src/component/ManUpage/Man_U.js
+++ src/component/ManUpage/Man_U.js
@@ -34,7 +34,7 @@ class Man_U extends Component {
           </div>
         </center>
         <center><h1 style={{ color: 'white' }}>Manchester United</h1></center>
-        <center><h2 style={{ color: 'white' }}>{products.length} Product</h2></center>
+        <center><h2 style={{ color: 'white' }}>{products.length} Products</h2></center>
 
         {/* Display the product data */}
         <div className="product-list">
